262f3934f0b5f4c9150fb96caef683b1318ac05f
tarek-berkane <riko.jsk.320@gmail.com>
2024-02-16 11:18:36 +0100

    add better exception handler

--- src/chargily_pay/api.py
+++ src/chargily_pay/api.py
@@ -6,13 +6,28 @@ import requests
 from requests.compat import urljoin
 
 from .entity import Checkout, Customer, PaymentLink, Price, Product
-from .settings import *
+from .settings import CHARGILIY_URL
 
 
 # drop None values
 exclude_none_value = lambda x: {k: v for (k, v) in x if v is not None}
 
 asdict_true_value = lambda x: asdict(x, dict_factory=exclude_none_value)
+
+
+def response_or_exception(fn):
+    from functools import wraps
+
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        response: requests.Response = fn(*args, **kwargs)
+        if response.status_code == 422:
+            raise requests.exceptions.HTTPError(response, response=response)
+        response.raise_for_status()
+
+        return response.json()
+
+    return wrapper
 
 
 class ChargilyClient:
@@ -32,22 +47,22 @@ class ChargilyClient:
     def get_balance(self):
         """Get your balance"""
         response = requests.get(urljoin(self.url, "balance"), headers=self.headers)
-        response.raise_for_status()
+
         return response.json()
 
     # ==================================
     # Customers
     # ==================================
-
-    def create_customer(self, customer: Customer):
+    @response_or_exception
+    def create_customer(self, customer: Customer, *args, **kwargs):
         """Create a customer"""
         customer_dict = asdict_true_value(customer)
         response = requests.post(
             urljoin(self.url, "customers"), headers=self.headers, json=customer_dict
         )
-        response.raise_for_status()
-        return response.json()
+        return response
 
+    @response_or_exception
     def update_customer(self, id, customer: Customer):
         """Update a customer"""
         customer_dict = asdict_true_value(customer)
@@ -56,17 +71,18 @@ class ChargilyClient:
             headers=self.headers,
             json=customer_dict,
         )
-        response.raise_for_status()
-        return response.json()
 
+        return response
+
+    @response_or_exception
     def retrieve_customer(self, id):
         """Retrieve a customer"""
         response = requests.get(
             urljoin(self.url, f"customers/{id}"), headers=self.headers
         )
-        response.raise_for_status()
-        return response.json()
+        return response
 
+    @response_or_exception
     def list_customers(self, per_page: int = 10, page: int = 1):
         """List customers"""
         response = requests.get(
@@ -74,21 +90,22 @@ class ChargilyClient:
             headers=self.headers,
             params={"per_page": per_page},
         )
-        response.raise_for_status()
-        return response.json()
 
+        return response
+
+    @response_or_exception
     def delete_customer(self, id):
         """Delete a customer"""
         response = requests.delete(
             urljoin(self.url, f"customers/{id}"), headers=self.headers
         )
-        response.raise_for_status()
-        return response.json()
+
+        return response
 
     # ==================================
     # Products
     # ==================================
-
+    @response_or_exception
     def create_product(self, product: Product):
         """Create a product"""
         product_dict = asdict_true_value(product)
@@ -98,9 +115,10 @@ class ChargilyClient:
             headers=self.headers,
             json=product_dict,
         )
-        response.raise_for_status()
-        return response.json()
 
+        return response
+
+    @response_or_exception
     def update_product(self, id, product: Product):
         """Update a product"""
         product_dict = asdict_true_value(product)
@@ -110,17 +128,19 @@ class ChargilyClient:
             headers=self.headers,
             json=product_dict,
         )
-        response.raise_for_status()
-        return response.json()
 
+        return response
+
+    @response_or_exception
     def retrieve_product(self, id):
         """Retrieve a product"""
         response = requests.get(
             urljoin(self.url, f"products/{id}"), headers=self.headers
         )
-        response.raise_for_status()
-        return response.json()
 
+        return response
+
+    @response_or_exception
     def list_products(self, per_page: int = 10, page: int = 1):
         """List products"""
         response = requests.get(
@@ -128,18 +148,19 @@ class ChargilyClient:
             headers=self.headers,
             params={"per_page": per_page},
         )
-        response.raise_for_status()
-        return response.json()
+
+        return response
 
     def delete_product(self, id):
         """Delete a product"""
         response = requests.delete(
             urljoin(self.url, f"products/{id}"), headers=self.headers
         )
-        response.raise_for_status()
-        return response.json()
+
+        return response
 
     # todo: retrieve product prices
+    @response_or_exception
     def retrieve_product_prices(self, id, per_page: int = 10, page: int = 1):
         """Retrieve product prices"""
         response = requests.get(
@@ -147,13 +168,14 @@ class ChargilyClient:
             headers=self.headers,
             params={"per_page": per_page},
         )
-        response.raise_for_status()
-        return response.json()
+
+        return response
 
     # ==================================
     # Prices
     # ==================================
 
+    @response_or_exception
     def create_price(self, price: Price):
         """Create a price"""
         price_dict = asdict_true_value(price)
@@ -162,9 +184,10 @@ class ChargilyClient:
             headers=self.headers,
             json=price_dict,
         )
-        response.raise_for_status()
-        return response.json()
 
+        return response
+
+    @response_or_exception
     def update_price(self, id, price: Price):
         """Update a price"""
         price_dict = asdict_true_value(price)
@@ -173,15 +196,17 @@ class ChargilyClient:
             headers=self.headers,
             json=price_dict,
         )
-        response.raise_for_status()
-        return response.json()
 
+        return response
+
+    @response_or_exception
     def retrieve_price(self, id):
         """Retrieve a price"""
         response = requests.get(urljoin(self.url, f"prices/{id}"), headers=self.headers)
-        response.raise_for_status()
-        return response.json()
 
+        return response
+
+    @response_or_exception
     def list_prices(self, per_page: int = 10, page: int = 1):
         """List prices"""
         response = requests.get(
@@ -189,13 +214,14 @@ class ChargilyClient:
             headers=self.headers,
             params={"per_page": per_page},
         )
-        response.raise_for_status()
-        return response.json()
+
+        return response
 
     # ==================================
     # Checkouts
     # ==================================
 
+    @response_or_exception
     def create_checkout(self, checkout: Checkout):
         """Create a checkout"""
         checkout_dict = asdict_true_value(checkout)
@@ -204,17 +230,19 @@ class ChargilyClient:
             headers=self.headers,
             json=checkout_dict,
         )
-        response.raise_for_status()
-        return response.json()
 
+        return response
+
+    @response_or_exception
     def retrieve_checkout(self, id):
         """Retrieve a checkout"""
         response = requests.get(
             urljoin(self.url, f"checkouts/{id}"), headers=self.headers
         )
-        response.raise_for_status()
-        return response.json()
 
+        return response
+
+    @response_or_exception
     def list_checkouts(self, per_page: int = 10, page: int = 1):
         """List checkouts"""
         response = requests.get(
@@ -223,9 +251,9 @@ class ChargilyClient:
             params={"per_page": per_page},
         )
 
-        response.raise_for_status()
-        return response.json()
+        return response
 
+    @response_or_exception
     def retrieve_checkout_items(self, id, per_page: int = 10, page: int = 1):
         """List checkouts items"""
         response = requests.get(
@@ -234,20 +262,21 @@ class ChargilyClient:
             params={"per_page": per_page},
         )
 
-        response.raise_for_status()
-        return response.json()
+        return response
 
+    @response_or_exception
     def expire_checkout(self, id):
         """Expire a checkout"""
         response = requests.post(
             urljoin(self.url, f"checkouts/{id}/expire"), headers=self.headers
         )
-        response.raise_for_status()
-        return response.json()
+
+        return response
 
     # ==================================
     # Payment Links
     # ==================================
+    @response_or_exception
     def create_payment_link(self, payment_link: PaymentLink):
         """Create a payment link"""
         payment_link_dict = asdict_true_value(payment_link)
@@ -256,10 +285,11 @@ class ChargilyClient:
             headers=self.headers,
             json=payment_link_dict,
         )
-        response.raise_for_status()
-        return response.json()
 
-    def updata_payment_link(self, id, payment_link: PaymentLink):
+        return response
+
+    @response_or_exception
+    def update_payment_link(self, id, payment_link: PaymentLink):
         """Update a payment link"""
         payment_link_dict = asdict_true_value(payment_link)
         response = requests.post(
@@ -267,17 +297,19 @@ class ChargilyClient:
             headers=self.headers,
             json=payment_link_dict,
         )
-        response.raise_for_status()
-        return response.json()
 
+        return response
+
+    @response_or_exception
     def retrieve_payment_link(self, id):
         """Retrieve a payment link"""
         response = requests.get(
             urljoin(self.url, f"payment-links/{id}"), headers=self.headers
         )
-        response.raise_for_status()
-        return response.json()
 
+        return response
+
+    @response_or_exception
     def list_payment_links(self, per_page: int = 10, page: int = 1):
         """List payment links"""
         response = requests.get(
@@ -286,9 +318,9 @@ class ChargilyClient:
             params={"per_page": per_page},
         )
 
-        response.raise_for_status()
-        return response.json()
+        return response
 
+    @response_or_exception
     def retrieve_payment_link_items(self, id, per_page: int = 10, page: int = 1):
         """List payment link items"""
         response = requests.get(
@@ -297,8 +329,7 @@ class ChargilyClient:
             params={"per_page": per_page},
         )
 
-        response.raise_for_status()
-        return response.json()
+        return response
 
     # ==================================
     # Utils
